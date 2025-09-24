77de7f0858a6cbc95f8cab15b60aa16bd0680441
Divya Arekatla <divyaare97@gmail.com>
2025-09-24 10:29:22 -0400

    message

--- mongodb.sh
+++ mongodb.sh
@@ -37,3 +37,9 @@ systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "enable mongodb"
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "start mongodb"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to mongodb"
+
+systemctl restart mongod &>>$LOG_FILE
+VLAIDATE $? "Restart mongodb"
