1241a04009faf8f46e80516405152592358ca4ce
Divya Arekatla <divyaare97@gmail.com>
2025-09-25 14:33:10 -0400

    message

---
+++ cart.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daws86s.com
+
+LOG_FILE="$LOGS_FOLDDER/$SCRIPT_NAME.log" ## /var/log/shell-roboshop/cart.log
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 ## failure is other than 0
+fi
+
+VALIDATE(){ # functions receive inputs through args just like shell script args
+    if [ $1 -ne 0 ]; then 
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 ... $R SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATE $? "disabling nodejs"
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATE $? "enabling nodejs:20"
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATE $? "installing nodejs"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0]; then 
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
+else
+    echo -e "User already exists ... $Y SKIPPING $N" &>>$LOG_FILE
+
+fi
+mkdir -p /app 
+VALIDATE $? "Creating app directory"
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
+VALIDATE $? "downloading cart application"
+cd /app 
+VALIDATE $? "changing to app directory"
+
+rm -rf /app/*
+VALIDATE $? "Removing existing code"
+unzip /tmp/cart.zip &>>$LOG_FILE
+VALIDATE $? "unzip cart"
+
+npm install &>>$LOG_FILE
+VALIDATE $? "installing dependencies"
+
+CP $SCRIPT_DIR/cart.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl enable cart &>>$LOG_FILE
+VALIDATE $? "enabling cart"
+systemctl start cart &>>$LOG_FILE
+VALIDATE $? "starting cart"
+
+
+
+
+
+
+
+
