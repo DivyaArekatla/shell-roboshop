acfcf5669bbfce96c0fcbdfb8850e3b959729755
Divya Arekatla <divyaare97@gmail.com>
2025-09-25 12:04:54 -0400

    message

--- redis.sh
+++ redis.sh
@@ -35,6 +35,7 @@ dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Installing redis"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connection to redis"
 systemctl enable redis 
 VALIDATE $? "enable redis"
 systemctl start redis 
